b2c6bdc6083d86262c90a5d7027442fb405c6411
Geo Gabriela <seuemail@gmail.com>
2025-10-30 21:04:54 -0300

    primeiro commit

---
+++ src/main/java/com/delivery_api/Project/Delivery/API/ProjectDeliveryApiApplication.java
@@ -0,0 +1,13 @@
+package com.delivery_api.Project.Delivery.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDeliveryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDeliveryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/delivery_api/Project/Delivery/API/ProjectDeliveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivery_api.Project.Delivery.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDeliveryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
